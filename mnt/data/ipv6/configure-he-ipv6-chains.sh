5cb1475f0e13174bfda19feb3b59b0eafaeb124b
Jason Hobbs <telnetdoogie@gmail.com>
2022-04-12 01:58:08 -0500

    Update configure-he-ipv6-chains.sh
    update path based on UDMP vs UDMP-SE

--- mnt/data/ipv6/configure-he-ipv6-chains.sh
+++ mnt/data/ipv6/configure-he-ipv6-chains.sh
@@ -1,7 +1,10 @@
-#!/bin/sh
+IP6TABLES_PATH="/usr/sbin"
+if test -f "/sbin/iptables-save"; then
+        IP6TABLES_PATH="/sbin"
+fi
 
 # export ip6tables, remove eth8 entries, replace eth9 entries with he-ipv6
-if /usr/sbin/ip6tables-save | grep -Fq "eth9" ; then
+if $IP6TABLES_PATH/ip6tables-save | grep -Fq "eth9" ; then
     echo "Updating ip6tables to replace eth9 with he-ipv6..."
-    /usr/sbin/ip6tables-save | sed '/eth8/d' | sed 's/eth9/he-ipv6/g' | /usr/sbin/ip6tables-restore
+    $IP6TABLES_PATH/ip6tables-save | sed '/eth8/d' | sed 's/eth9/he-ipv6/g' | $IP6TABLES_PATH/ip6tables-restore
 fi
