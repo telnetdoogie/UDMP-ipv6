4d8ea7657f3e199688a5e19cb26db462ebb1c431
Jason Hobbs <telnetdoogie@gmail.com>
2022-04-12 02:17:44 -0500

    Update 41-enable-he-ipv6.sh
    parameterize delay and create more verbose output for logs

--- mnt/data/on_boot.d/41-enable-he-ipv6.sh
+++ mnt/data/on_boot.d/41-enable-he-ipv6.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+DELAY=60
 
 #delay start, since networking is still not started yet
-sleep 120 && /mnt/data/ipv6/enable-he-ipv6.sh &
+echo "Adding he-ipv6 tunnel in $DELAY seconds..." && sleep $DELAY && echo "Adding he-ipv6 tunnel..." && /mnt/data/ipv6/enable-he-ipv6.sh &
