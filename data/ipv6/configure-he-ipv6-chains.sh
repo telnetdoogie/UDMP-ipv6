9b6feb41ec330c6afa3527f8884ebaf9d1350d0a
Jason Hobbs <telnetdoogie@gmail.com>
2023-01-31 23:19:16 -0600

    Update configure-he-ipv6-chains.sh

--- data/ipv6/configure-he-ipv6-chains.sh
+++ data/ipv6/configure-he-ipv6-chains.sh
@@ -6,7 +6,7 @@ if test -f "/sbin/iptables-save"; then
         IP6TABLES_PATH="/sbin"
 fi
 
-# export ip6tables, remove eth8 entries, replace eth9 entries with he-ipv6
+# export ip6tables, remove eth8 entries, replace eth9 entries with he-ipv6, and add
 if $IP6TABLES_PATH/ip6tables-save | grep -Fq "eth9" ; then
     echo "Updating ip6tables to replace eth9 with he-ipv6..."
     $IP6TABLES_PATH/ip6tables-save | sed '/eth8/d' | sed 's/eth9/he-ipv6/g' | $IP6TABLES_PATH/ip6tables-restore
